784bb3ca887dd14591444847f3875f59f3e3051f
Rodrigopca42 <rodrigopca426281@gmail.com>
2022-12-27 12:25:39 -0300

    Projeto iniciado

---
+++ src/test/java/com/rodrigo/hekpdesk/HelpdeskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.hekpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
