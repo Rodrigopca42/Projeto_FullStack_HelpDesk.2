b954a95294a2eecd88a685442c816606948ad7ee
Rodrigopca42 <rodrigopca426281@gmail.com>
2023-01-03 15:10:43 -0300

    CRUD para cliente implementado

---
+++ src/main/java/com/rodrigo/hekpdesk/services/ClienteService.java
@@ -0,0 +1,92 @@
+package com.rodrigo.hekpdesk.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigo.hekpdesk.domain.Pessoa;
+import com.rodrigo.hekpdesk.domain.Cliente;
+import com.rodrigo.hekpdesk.domain.dtos.ClienteDTO;
+import com.rodrigo.hekpdesk.repositories.PessoaRepository;
+import com.rodrigo.hekpdesk.repositories.ClienteRepository;
+import com.rodrigo.hekpdesk.services.exceptions.DataIntegrityViolationException;
+import com.rodrigo.hekpdesk.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repository;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(()-> new ObjectnotFoundException("Objeto não encontrado! ID: "+ id));
+	}
+
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+
+	public Cliente create(ClienteDTO objDTO) {
+		objDTO.setId(null);
+		ValidaPorCpfEEmail(objDTO);
+		Cliente newObj = new Cliente(objDTO);
+		return repository.save(newObj);
+	}
+	
+	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+		objDTO.setId(id);
+		Cliente oldObj = findById(id);
+		ValidaPorCpfEEmail(objDTO);
+		oldObj = new Cliente(objDTO);
+		return repository.save(oldObj);
+	}
+	
+	public void delete(Integer id) {
+		Cliente obj = findById(id);
+		if(obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Cliente possui ordens de serviço e não pode ser deletado!");
+		}
+			repository.deleteById(id);
+		
+	}
+
+
+	private void ValidaPorCpfEEmail(ClienteDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema");
+		}
+			
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-MAIL já cadastrado no sistema");
+		}
+		
+	}
+
+	
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
