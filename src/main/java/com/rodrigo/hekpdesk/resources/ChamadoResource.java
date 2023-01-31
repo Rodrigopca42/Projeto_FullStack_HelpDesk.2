58061bac83533f89846a7d44daf43081665b2449
Rodrigopca42 <rodrigopca426281@gmail.com>
2023-01-31 14:55:56 -0300

    FindById para Chamado

---
+++ src/main/java/com/rodrigo/hekpdesk/resources/ChamadoResource.java
@@ -0,0 +1,26 @@
+package com.rodrigo.hekpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rodrigo.hekpdesk.domain.Chamado;
+import com.rodrigo.hekpdesk.domain.dtos.ChamadoDTO;
+import com.rodrigo.hekpdesk.services.ChamadoService;
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoResource {
+
+	@Autowired
+	private ChamadoService service;
+		
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
+		Chamado obj = service.findById(id);
+		return ResponseEntity.ok().body(new ChamadoDTO(obj));
+	}
+}
---
+++ src/main/java/com/rodrigo/hekpdesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.rodrigo.hekpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigo.hekpdesk.domain.Chamado;
+import com.rodrigo.hekpdesk.repositories.ChamadoRepository;
+import com.rodrigo.hekpdesk.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class ChamadoService {
+
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID: "+ id));
+	}
+}
