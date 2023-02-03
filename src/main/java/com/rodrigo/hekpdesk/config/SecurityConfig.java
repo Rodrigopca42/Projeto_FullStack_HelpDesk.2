24e4e39f0465de3289978282d4aae83abb98194a
Rodrigopca42 <rodrigopca426281@gmail.com>
2023-02-03 14:41:02 -0300

    Autorização com  JWT

--- src/main/java/com/rodrigo/hekpdesk/config/SecurityConfig.java
+++ src/main/java/com/rodrigo/hekpdesk/config/SecurityConfig.java
@@ -18,10 +18,12 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.rodrigo.hekpdesk.security.JWTAuthenticationFilter;
+import com.rodrigo.hekpdesk.security.JWTAuthorizationFilter;
 import com.rodrigo.hekpdesk.security.JWTUtil;
 
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
@@ -40,7 +42,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		}
 
 		http.cors().and().csrf().disable();
+		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 
 		http.authorizeRequests()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
